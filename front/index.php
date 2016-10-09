935aa65bc05bda37d5a4b2933a42863f1ceea131
mwisniewicz <m.wisniewicz@gmail.com>
2016-10-09 22:22:09 +0200

    Upload file structure

--- front/index.php
+++ front/index.php
@@ -1,1 +1,70 @@
-<?php echo 'Yo!'; ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+      	<link rel="stylesheet" type="text/css" href="http://dc-js.github.io/dc.js/css/dc.css"/>
+        <script type="text/javascript" src="http://dc-js.github.io/dc.js/js/d3.js"></script>
+        <script type="text/javascript" src="http://dc-js.github.io/dc.js/js/crossfilter.js"></script>
+        <script type="text/javascript" src="http://dc-js.github.io/dc.js/js/dc.js"></script>
+
+    	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+
+<!-- ******* HEADER SECTION ********* -->
+<header>
+	<div class="container">
+
+	<h2>ObsAssistant <small>1.0.0</small></h2>
+
+	</div>
+
+</header>
+
+<!-- ******* MAIN SECTION ********* -->
+<div class="charts-section">
+	<div class="container">
+
+		<div class="row">
+
+			<h3>Charts</h3>
+
+		</div>
+
+	</div>
+</div>
+
+<!-- ******* FOOTER SECTION ********* -->
+<footer>
+	<div class="container">
+			<div class="row">
+
+				<div class="col-lg-6 col-md-6">
+
+					Footer 1
+
+				</div>
+
+				<div class="col-lg-6 col-md-6">
+
+					Footer 2
+
+				</div>
+
+			</div>
+
+			<div class="row">
+				<div class="copy">
+				Copyright 2016
+				</div>
+		</div>
+	</div>
+</footer>
+</body>
+</html>
