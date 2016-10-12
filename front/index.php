b488ae5948a9d280512ad8cf6cb3d3735f711fe6
mwisniewicz <m.wisniewicz@gmail.com>
2016-10-12 21:18:55 +0200

    remove html file

--- front/index.php
+++ front/index.php
@@ -50,7 +50,7 @@
 
 				<div class="col-lg-6 col-md-6">
 
-					Footer 1
+					<p>Program for show in real time during the observations FLUX, SNR, FWHM and position of observed object. Program can help centered object on image (in future this will be automatically), make good focus, watch good SNR etc.</p>
 
 				</div>
 
