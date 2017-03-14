fe78242037fec10ac79defd2b07850de3a854967
Michał Żejmo <michalzejmo@gmail.com>
2017-03-14 19:18:13 +0100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -2,6 +2,6 @@
 
 while true ; do
 sshpass -p <pass> rsync -c --temp-dir="/home/pi/Temp/rsync/data/temp/" zielona@beta:/home/zielona/.jastrocam3/preview.fits ~/Temp/rsync/data/
-#sshpass -p VW86Jetta rsync -c --temp-dir="/home/pi/Temp/rsync/data/temp/" zielona@beta:/home/zielona/dane/20151206/*.fits ~/Temp/rsync/data/
-sleep 1 # or however many seconds you like
+#sshpass -p <PASSWORD> -c --temp-dir="/home/pi/Temp/rsync/data/temp/" zielona@beta:/home/zielona/dane/20151206/*.fits ~/Temp/rsync/data/
+sleep 1 
 done
