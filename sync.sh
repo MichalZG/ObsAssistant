bb641fa0edd5b9a81beec96cb46eaa3dfa1d2ca3
Greg Stachowski <greg@astro.as.up.krakow.pl>
2018-08-05 00:27:46 +0200

    - Use temporary directory with unique name under /tmp. - Use user directory specified on command line or $HOME. - Clean up temporary directory after CTRL-C.

--- sync.sh
+++ sync.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 eval "$(ssh-agent -s)"
 ssh-add
-while true ; do
-rsync -c --temp-dir="/home/ogloza/Programs/ObsAssistant/temp/temp/" observer@epsilon:/home/observer/.jastrocam3/preview.fits /home/ogloza/Programs/ObsAssistant/temp/data/
-sleep 1 # or however many seconds you like
+tmpdir=`mktemp -d`
+if [ $# -gt 0 ]; then
+  destdir=$1
+else
+  destdir=$HOME
+fi
+trap ":" INT
+
+seconds=10 # or however many seconds you like
+
+while sleep $seconds ; do
+  rsync -c --progress --temp-dir="$tmpdir/" observer@epsilon:/home/observer/.jastrocam3/preview.fits $destdir
 done
+\rm -fr $tmpdir
