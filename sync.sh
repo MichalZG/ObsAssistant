d1504bcc77d21b489b863f92eaadc6850d886c06
root <root@beta.localdomain>
2019-12-14 23:31:17 +0100

    time delta ut1 = 0
    Committer: MichalZG <michalzejmo@gmail.com>

--- sync.sh
+++ sync.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+if [ ! -e ~/.ssh/id_eps_rsa ]; then
+	mkdir -p ~/.ssh/
+	TEMP=$(mktemp -d)
+	scp observer@epsilon:.ssh/id_rsa* $TEMP/
+	mv $TEMP/id_rsa ~/.ssh/id_eps_rsa
+	mv $TEMP/id_rsa.pub ~/.ssh/id_eps_rsa.pub
+	rm -rf $TEMP
+fi  
+
+
 eval "$(ssh-agent -s)"
-ssh-add
+ssh-add ~/.ssh/id_eps_rsa
+
 tmpdir=`mktemp -d`
 if [ $# -gt 0 ]; then
   destdir=$1
